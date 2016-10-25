c800a0edc8d3fe7f234a6e533da763d93359c5f6
Marco Velasquez <marco.velasquez@outlook.com>
2016-10-25 13:32:56 -0500

    first commit

---
+++ Owin.SelfHost1/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin.SelfHost1
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.Use(async (ctx, next) =>
+           {
+               await ctx.Response.WriteAsync("Web Content through the pipeline");
+           });
+        }
+    }
+}
